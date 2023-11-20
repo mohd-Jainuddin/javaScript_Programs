cb1d394fa2224ade017c198f6c763244d90f85fc
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2023-11-20 10:08:33 +0530

    five program

---
+++ five.js
@@ -0,0 +1,13 @@
+// Write a program to swap value of two variables without using third variable?
+
+let a = 5;
+let b = 6;
+
+console.log('a =',a,'b =',b);
+
+a = a+b;
+b = a-b;
+a = a-b;
+
+console.log('a =',a,'b =',b);
+
